5dab0527ee6ecd14a067ca3842463da2adbfe776
Radaxo31 <harshitchaurasia153@gmail.com>
2025-09-03 02:21:47 +0530

    Stating Javascript

---
+++ js1.js
@@ -0,0 +1,10 @@
+alert("Hello box");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+
+var a=prompt("Enter a number")
+
+console.log("Your number is "+a)
+
+document.title="Hello hola"
