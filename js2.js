4553928bbfd79719b679fc9eb42ca82a45367d30
Harshit Chaurasia <harshitchaurasia153@gmail.com>
2025-09-03 19:23:08 +0530

    Data types and variables

---
+++ js2.js
@@ -0,0 +1,45 @@
+console.log("The code is running")
+let a=10;
+let b=2;
+let c="Harry"
+console.log(a+b+8)
+console.log(typeof(c))
+{
+    let a=55;
+    console.log(a)
+}
+console.log(a)
+// console.log(d) cannot be used outside the block defined in
+const a1=7; //cannot be changed once assigned value
+// var 44b=10; not allowed
+let d =3;
+let e=5.56;
+const f=true;
+let g="Hello";
+let h=null;
+let i=undefined;
+console.log(d,e,f,g,h,i)
+console.log(typeof d,typeof e,typeof f,typeof g,typeof h,typeof i)
+let o={
+    name:"Harshit",
+    "phone number":8178774568,
+    marks:97
+}
+console.log(o)
+o.admno=2024;
+console.log(o)
+o.admno=202401;
+console.log(o)
+o.is_Hadsome=true;
+console.log(o)
+let x="Name";
+console.log(x+1)
+console.log(typeof x+1)
+let y={
+    Accentuate:"To make more noticeable or prominent.",
+    Ephemeral:"Lasting for a very short time; short-lived",
+    Idiosyncratic:"Peculiar or unique to an individual.",
+    Lucid:"Clear and easy to understand.",
+    Respite:" A pause from doing something or a period of rest. "
+}
+console.log(y)
