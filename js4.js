7f84a532a4ba9a683b48c403a0c7289a7f931deb
Radaxo31 <harshitchaurasia153@gmail.com>
2025-09-04 23:42:29 +0530

    Loops

---
+++ js4.js
@@ -0,0 +1,30 @@
+console.log("I am learning loops")
+let a=1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+// let obj={
+//     Name:"Harshit",
+//     Rollno:"2400320100505",
+//     Class:"CSE-17"
+// }
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element);
+// }
+//     for (const c of "Harshit") {
+//         console.log(c);
+// }
+// let i=4;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+let i=6;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
