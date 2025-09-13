c98fae21c75f5e053014bc9b19ea3a64fec3fa4f
Radaxo31 <harshitchaurasia153@gmail.com>
2025-09-14 00:37:46 +0530

    Async await use cases

---
+++ js13.js
@@ -0,0 +1,31 @@
+// async function getdata() {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(455)
+//       }, 3000);
+//     }
+//     )
+// }
+async function getdata() {
+    let x=await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let data = await x.json()
+    console.log(data)
+    return 455
+}
+async function main(){
+    console.log("Loading MOdules");
+    console.log("Do something else")
+    console.log("Load Data")
+    let data = await getdata();
+    console.log(data)
+    console.log("process data")
+    console.log("task 2")
+}
+main()
+// data.then ((e) => {
+  
+
+// console.log(data)
+
+// console.log("Process data")
+// })
